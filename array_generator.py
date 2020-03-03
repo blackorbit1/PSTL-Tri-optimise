a51fc037d8461f7f8b4ec906f1b99448f669c787
Enzo Dutra <38140797+blackorbit1@users.noreply.github.com>
2020-03-04 00:38:46 +0100

    run generation with given entropy + dynamic parameters
    Pour les paramètres dynamiques, seule la taille de la liste est fonctionnelle avec les variables suivantes :
    
    t = taille de la liste précédente (taille début pour la première)
    tf = taille_fin
    td = taille_fin
    nb = nombre total de listes
    n = numéro de liste

--- array_generator.py
+++ array_generator.py
@@ -4,6 +4,7 @@ configuration = json.load(open('array_generator_config.json'))
 
 methode = configuration["methode_tri"]
 nb_listes = configuration["nb_listes"]
+taille_formule = configuration["taille"]
 taille_depart = configuration["taille_depart"]
 taille_fin = configuration["taille_fin"]
 taille_liste = taille_depart
@@ -11,6 +12,8 @@ taille_liste = taille_depart
 nb_runs_depart = configuration["nb_runs_depart"]
 nb_runs_fin = configuration["nb_runs_fin"]
 nb_runs = nb_runs_depart
+
+anthropie = configuration["anthropie"]
 
 borne_sup = configuration["borne_sup"]
 borne_inf = configuration["borne_inf"]
@@ -21,15 +24,24 @@ decroissant = bool(configuration["contiens_runs_decroissants"])
 fichier_path = open("paths","w+")
 
 for num_liste in range(nb_listes):
-    taille_liste = int(taille_liste + (taille_fin - taille_depart)/nb_listes)
-    nb_runs = int(nb_runs + (nb_runs_fin - taille_depart)/nb_listes)
+    #taille_liste = int(taille_liste + (taille_fin - taille_depart)/nb_listes)
+    t = taille_liste
+    tf = taille_fin
+    td = taille_fin
+    nb = nb_listes
+    n = num_liste # variable utilisé dans le fichier json de configuration
+    taille_liste = int(eval(taille_formule))
+    nb_runs = int(nb_runs + (nb_runs_fin - nb_runs_depart)/nb_listes)
 
     liste = [None]*taille_liste
 
     if methode == "alea" :
         for i in range(len(liste)):
             liste[i] = random.randint(borne_inf, borne_sup)
-    elif methode == "parti_tri_run_constant":
+
+
+    elif methode == "parti_tri_run_constant_croiss_lineaires":
+
         taille_runs = taille_liste / nb_runs
         increment = 0
         run_decroissant = False
@@ -48,8 +60,48 @@ for num_liste in range(nb_listes):
                 max_acc = max_acc + distance_var
             increment = increment + taille_runs
             run_decroissant = False
+
+
+    elif methode == "parti_tri_run_non_constant":
+
+        liste_separation_runs = [0, taille_liste]
+        for _ in range(nb_runs - 1 if nb_runs > 0 else 0):
+            liste_separation_runs.append(random.randint(0, taille_liste))
+        liste_separation_runs.sort()
+        for i in range(len(liste_separation_runs) - 1):
+            for j in range(liste_separation_runs[i], liste_separation_runs[i+1]):
+                liste[j] = random.randint(borne_inf, borne_sup)
+            sub_list = sorted(liste[liste_separation_runs[i]:liste_separation_runs[i+1]])
+            if(decroissant and random.randint(0, 1)):
+                liste[liste_separation_runs[i]:liste_separation_runs[i+1]] = reversed(sub_list)
+            else:
+                liste[liste_separation_runs[i]:liste_separation_runs[i+1]] = sub_list
+
+
+    elif methode == "parti_tri_entropie":
+        # "séparation" = bornes des runs
+
+        # On crée une liste de séparation parfaite qui nous servira de référence pour choisir l'anthropie
+        liste_separation_runs_parfaite = [i for i in range(0, (taille_liste), int(taille_liste/nb_runs))]
+        liste_separation_runs_parfaite[-1] = taille_liste
+
+        # On met les deux extremums de la liste des séparations
+        liste_separation_runs = [0, taille_liste]
+        for i in range(1, nb_runs - 0 if nb_runs > 0 else 0):
+            separation_min = liste_separation_runs_parfaite[i] - (liste_separation_runs_parfaite[i] * anthropie / 100) #0 if (i - 1) < 0 else liste_separation_runs_parfaite[i-1]
+            separation_max = liste_separation_runs_parfaite[i] + ((liste_separation_runs_parfaite[-1] - liste_separation_runs_parfaite[i]) * anthropie / 100)
+            liste_separation_runs.append(random.randint(int(separation_min), int(separation_max)))
+        
+        # On tri les séparations dans le cas où l'anthropie ait permis des cheuvauchements de bornes
+        liste_separation_runs.sort()
+
+        # On crée la liste finale en remplissant les runs
+        for i in range(len(liste_separation_runs) - 1):
+            for j in range(liste_separation_runs[i], liste_separation_runs[i+1]):
+                liste[j] = random.randint(borne_inf, borne_sup)
+            liste[liste_separation_runs[i]:liste_separation_runs[i+1]] = sorted(liste[liste_separation_runs[i]:liste_separation_runs[i+1]])
     else:
-        print("Aucune méthode n'a été précisé")
+        print("Aucune méthode valide n'a été précisé")
         exit()
 
     # écriture de la liste dans un fichier
