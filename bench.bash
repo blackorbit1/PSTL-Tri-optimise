eb5d1913b23a8c8b69609b8fad915aa90fcc13ba
pintob <pintob_info@yahoo.fr>
2020-03-24 13:52:50 +0100

    commit

--- bench.bash
+++ bench.bash
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-mkdir res
-mkdir tab
+mkdir -p build
+cmake -DCMAKE_BUILD_TYPE=Release -B build
 make
+
+mkdir -p res
+mkdir -p tab
+
 python array_generator.py >/dev/null
+
 for i in tab/*
 do
   echo bench of $i
