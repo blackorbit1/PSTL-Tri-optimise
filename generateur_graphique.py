6a42878747a9eeb9b85df1b920740f930ce35880
blackorbit1 <enzo.dutra@gmail.com>
2020-05-21 22:59:15 +0200

    ajout heatmap pour chaque algo à utiliser via "new_heatmap"

--- generateur_graphique.py
+++ generateur_graphique.py
@@ -4,15 +4,22 @@ import seaborn as sns
 import pandas as pd
 import math
 import numpy as np
+import entropy_utils as e
+from statistics import mean
+
 from operator import itemgetter
 
 if len(sys.argv) < 2:
     print("--------------------------------------------------------")
-    print("   Usage: $ " + sys.argv[0] + " param_1 [param_n]")
+    print("   Usage: $ " + sys.argv[0] + " param_1 [param_n] [-s --size]")
     print(" ")
     print("   * param_1: name of a graphic you want")
     print("   * ...")
     print("   * param_n: name of a graphic you want")
+    print(" ")
+    print("   * -s / --size integer_1 ... integer_n : all list size you want to display")
+    print("                                           otherwise, every size will be mixed")
+    print("                                           (only for temps/entropie graph)")
     print("--------------------------------------------------------")
     exit(0)
 
@@ -24,7 +31,7 @@ nb_algos = 0
 
 for path in open('paths'):
     try:
-        res_path = str(path).replace("/tab/", "/res/").split("\n")[0]
+        res_path = str(path).replace("/tab/", "/res_lip6/").split("\n")[0]
         configuration = json.load(open(res_path))
 
         nb_algos = len(configuration["content"]) - len(algos_a_retirer)
@@ -98,6 +105,208 @@ for param, n in zip(sys.argv[1:], range(1, len(sys.argv))):
         pyplot.savefig('temps_taille.png')
 
 
+    if param == "new_temps/taille":
+        pyplot.figure(param)
+        liste_benchs_entropie = []
+
+
+
+        taille = 0
+        boites = dict()
+        for key in liste_benchs:
+            liste_benchs_taille = sorted(liste_benchs[key], key=lambda k: float(k['taille_liste']))
+            i = 0
+            for bench in liste_benchs_taille:
+                taille = float(bench["taille_liste"])
+
+                if not key in boites :
+                    boites[key] = dict()
+                if not taille in boites[key]:
+                    boites[key][taille] = dict()
+                boites[key][taille][i] = float(bench["time"])/1000000
+
+                i += 1
+
+        temps = list()
+        for key in boites.keys():
+            for key, val in boites[key].items():
+                temps += list(val.values())
+
+        algos = list()
+        for key1 in boites.keys():
+            for key2, val in boites[key1].items():
+                algos += [key1] * len(val.values())
+
+        taille = list()
+        for key1 in boites.keys():
+            for key2, val in boites[key1].items():
+                taille += [key2] * len(val.values())
+
+
+        df = pd.DataFrame({'Algorithme': algos,
+                           'Taille': taille,
+                           'Temps': temps })
+
+        ax = sns.boxplot(x='Taille', hue='Algorithme', y='Temps', data=df, showcaps = False, whis = [0, 100], linewidth=0.5)
+
+        pyplot.savefig('new_temps_taille.png')
+
+    if param == "heatmap":
+        pyplot.figure(param)
+        liste_benchs_entropie = []
+
+
+
+        taille = 0
+        boites = dict()
+        for key in liste_benchs:
+            liste_benchs_entropie = sorted(liste_benchs[key], key=lambda k: float(k['entropie']), reverse=False)
+
+            i = 0
+            for bench in liste_benchs_entropie:
+                taille = float(bench["taille_liste"])
+                entro = float(bench["entropie"])
+
+                max_entropy = e.get_entropy_from_nb_runs(taille)
+                taux_entropie = int(round(entro / max_entropy, 1) * 10)
+
+                print("taux_entropie : ", taux_entropie)
+
+                if not key in boites :
+                    boites[key] = dict()
+                if not taux_entropie in boites[key]:
+                    boites[key][taux_entropie] = dict()
+                if not taille in boites[key][taux_entropie]:
+                    boites[key][taux_entropie][taille] = dict()
+
+                boites[key][taux_entropie][taille][i] = float(bench["time"])/1000000
+
+                i += 1
+        taille_listes = len(list(list(list(boites.values())[0].values())[0].values())[0])
+        tableau = list()#[[0]*20]*20
+        for j in range(4):
+            tableau.append(list())
+            for i in range(10):
+                temps_tab = list()
+                for y in range(20):
+                    temps_tab.append(0)
+                tableau[j].append(temps_tab)
+        x = 0
+        y = 0
+        i = 0
+        for key1 in boites.keys():
+            for key2, val in boites["AdaptativeShiverSort"].items():
+                print(key2)
+
+                for key3, val2 in boites[key1][key2].items():
+                    #print(x, y)
+                    #print(int(key3/5000)-1, key2, mean(list(val2.values())))
+                    tableau[i][key2][int(key3/5000)-1] = mean(list(val2.values()))
+                    y += 1
+                x += 1
+                y = 0
+            i += 1
+
+        print(tableau)
+
+
+        for i in range(len(tableau)):
+            pyplot.figure(param)
+            ax = sns.heatmap(tableau[i], linewidth=0, cmap="inferno")
+            ax.invert_yaxis()
+            pyplot.savefig('heatmap' + str(i) + '.png')
+
+        exit()
+
+        pyplot.show()
+
+    if param == "new_heatmap":
+
+        algos_to_display = list()
+        for algo in liste_benchs:
+            algos_to_display.append(algo)
+
+
+
+        pyplot.figure(param)
+        liste_benchs_entropie = []
+
+
+
+        columns = max(1, math.ceil(math.sqrt(len(algos_to_display))))
+        rows = max(1, (columns - 1) if (columns - 1)*columns >= len(algos_to_display) else columns)
+        print(columns, rows)
+        fig, axes = pyplot.subplots(rows,columns,figsize=[10,10],frameon = False)
+        if not type(axes) == np.ndarray :
+            if type(axes) == tuple:
+                axes = [list(axes)]
+            else :
+                axes = [[axes]]
+
+        pyplot.tight_layout()
+        pyplot.subplots_adjust(left=None, bottom=None, right=None, top= 0.9, wspace=0.25 , hspace=0.2)
+
+        liste_sous_graphes = list()
+        d = {}
+        i = 0
+
+        for r in range(rows):
+            for c in range(columns):
+                d[i] = axes[r][c]
+                i += 1
+
+        nb_sous_graphes = max(len(algos_to_display), 1)
+        data = [dict() for _ in range(nb_sous_graphes)]
+
+        for n, algo in zip(range(nb_sous_graphes), algos_to_display):
+
+            entro = 0
+            iteration = 1
+            data[n]["boites"] = dict()
+            for key in liste_benchs:
+                liste_benchs_entropie = sorted(liste_benchs[key], key=lambda k: float(k['entropie']))
+                i = 0
+                for bench in liste_benchs_entropie:
+                    taille = float(bench["taille_liste"])
+                    entro = float(bench["entropie"])
+
+                    max_entropy = e.get_entropy_from_nb_runs(taille)
+                    taux_entropie = int(round(entro / max_entropy, 1) * 10)
+
+                    if not key in data[n]["boites"] :
+                        data[n]["boites"][key] = dict()
+                    if not taux_entropie in data[n]["boites"][key]:
+                        data[n]["boites"][key][taux_entropie] = dict()
+                    if not taille in data[n]["boites"][key][taux_entropie]:
+                        data[n]["boites"][key][taux_entropie][taille] = dict()
+
+                    data[n]["boites"][key][taux_entropie][taille][i] = float(bench["time"])/1000000
+
+                    i += 1
+
+            taille_listes = len(list(list(list(data[n]["boites"].values())[0].values())[0].values())[0])
+            data[n]["tableau"] = list()#[[0]*20]*20
+            for i in range(10):
+                temps_tab = list()
+                for y in range(20):
+                    temps_tab.append(0)
+                data[n]["tableau"].append(temps_tab)
+            x = 0
+            y = 0
+            i = 0
+
+            for key, val in data[n]["boites"][algo].items():
+                for key3, val2 in data[n]["boites"][algo][key].items():
+                    data[n]["tableau"][key][int(key3/5000)-1] = mean(list(val2.values()))
+
+
+
+            data[n]["ax"] = sns.heatmap(data[n]["tableau"], linewidth=0, cmap="inferno", ax = d[n])
+            data[n]["ax"].invert_yaxis()
+            d[n].set_title("Performances de l'algorithme " + str(algos_to_display[n]))
+
+        pyplot.savefig('new_heatmap.png')
+
     if param == "old_temps/entropie":
         pyplot.figure(param)
         #print(liste_benchs)
@@ -129,22 +338,30 @@ for param, n in zip(sys.argv[1:], range(1, len(sys.argv))):
 
     if param == "temps/entropie":
         size_to_display = list()
-        if (n+1) < len(sys.argv) and (sys.argv[n+1] == "--size" or sys.argv[n+1] == "-s") :
-            size_index = 0
-            while (n + size_index + 2) < len(sys.argv) and sys.argv[n + size_index + 2].isnumeric():
-                size_to_display.append(int(sys.argv[n + size_index + 2]))
-                size_index += 1
+        for arg, i in zip(sys.argv, [x for x in range(len(sys.argv))]):
+            if (n+1) < len(sys.argv) and (arg == "--size" or arg == "-s") and (i+1) < len(sys.argv) and sys.argv[i+1].isnumeric():
+                size_index = 0
+                while (n + size_index + 2) < len(sys.argv) and sys.argv[n + size_index + 2].isnumeric():
+                    size_to_display.append(int(sys.argv[n + size_index + 2]))
+                    size_index += 1
+
+
 
         pyplot.figure(param)
         liste_benchs_entropie = []
 
 
 
-        columns = math.ceil(math.sqrt(len(size_to_display)))
-        rows = (columns - 1) if (columns - 1)*columns >= len(size_to_display) else columns
+        columns = max(1, math.ceil(math.sqrt(len(size_to_display))))
+        rows = max(1, (columns - 1) if (columns - 1)*columns >= len(size_to_display) else columns)
+        print(columns, rows)
         fig, axes = pyplot.subplots(rows,columns,figsize=[10,10],frameon = False)
-        if not type(axes) == np.ndarray:
-            axes = [list(axes)]
+        if not type(axes) == np.ndarray :
+            if type(axes) == tuple:
+                axes = [list(axes)]
+            else :
+                axes = [[axes]]
+
         pyplot.tight_layout()
         pyplot.subplots_adjust(left=None, bottom=None, right=None, top= 0.9, wspace=0.25 , hspace=0.2)
 
@@ -200,13 +417,12 @@ for param, n in zip(sys.argv[1:], range(1, len(sys.argv))):
 
 
 
-
             data[n]["df"] = pd.DataFrame({'Algorithme': data[n]["algos"],
                                'Entropie': data[n]["entropy"],
                                'Temps': data[n]["temps"] })
 
 
-            data[n]["ax"] = sns.boxplot(ax = d[n], x='Entropie', hue='Algorithme', y='Temps', data=data[n]["df"], showcaps = False, whis = [0, 100], linewidth=0.5)
+            data[n]["ax"] = sns.boxplot(ax = d[n], x='Entropie', hue='Algorithme', y='Temps', data=data[n]["df"], showcaps = False, linewidth=0.5)
             if nb_sous_graphes > 1 :
                 d[n].set_title("listes de taille " + str(size_to_display[n]))
             else :
